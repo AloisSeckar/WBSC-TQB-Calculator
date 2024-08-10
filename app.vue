6787b488921a96238eeb315f594709dc81b81ec8
Alois Sečkár <ellrohir@seznam.cz>
2024-08-10 23:45:50 +0200

    feat: UI structure stub

--- app.vue
+++ app.vue
@@ -1,9 +1,33 @@
 <template>
   <div>
-    <h1>WBSC TQB Calculator</h1>
-    <div>Online tool for qucikly calculating TQB method at WBSC tournaments</div>
+    <h1 class="mb-4 text-2xl font-bold">
+      WBSC TQB Calculator
+    </h1>
+    <div class="mb-4">
+      Online tool for qucikly calculating TQB method at WBSC tournaments
+    </div>
     <div>
-      TQB CALC
+      Insert teams:
+    </div>
+    <div v-for="team in teams" :key="team.name" class="mb-2 flex flex-row gap-2 items-center">
+      <UInput v-model="team.name" />
+    </div>
+    <div>
+      Insert scores:
+    </div>
+    <div v-for="game in games" :key="game.id" class="mb-2 flex flex-row gap-2 items-center">
+      Away:
+      <UInput v-model="game.awayTeam" />
+      Away PTS:
+      <UInput v-model="game.awayScore" type="number" />
+      Away IP:
+      <UInput v-model="game.awayIP" type="number" />
+      Home:
+      <UInput v-model="game.homeTeam" />
+      Home PTS:
+      <UInput v-model="game.homeScore" type="number" />
+      Home IP:
+      <UInput v-model="game.homeIP" type="number" />
     </div>
   </div>
 </template>
@@ -15,4 +39,36 @@ useHead({
     lang: 'en',
   },
 })
+
+const teams: Ref<Team[]> = ref([{name: 'Team 1'}, {name: 'Team 2'}, {name: 'Team 3'}])
+
+const games: Game[] = [
+  {
+    id: 1,
+    awayTeam: teams.value[0]!.name,
+    awayScore: 0,
+    awayIP: 0,
+    homeTeam: teams.value[1]!.name,
+    homeScore: 0,
+    homeIP: 0,
+  }, 
+  {
+    id: 2,
+    awayTeam: teams.value[1]!.name,
+    awayScore: 0,
+    awayIP: 0,
+    homeTeam: teams.value[2]!.name,
+    homeScore: 0,
+    homeIP: 0,
+  }, 
+  {
+    id: 3,
+    awayTeam: teams.value[2]!.name,
+    awayScore: 0,
+    awayIP: 0,
+    homeTeam: teams.value[0]!.name,
+    homeScore: 0,
+    homeIP: 0,
+  }
+]
 </script>
---
+++ utils/types.ts
@@ -0,0 +1,13 @@
+export type Team = {
+  name: string
+}
+
+export type Game = {
+  id: number
+  awayTeam: string
+  awayScore: number
+  awayIP: number
+  homeTeam: string
+  homeScore: number
+  homeIP: number
+}
