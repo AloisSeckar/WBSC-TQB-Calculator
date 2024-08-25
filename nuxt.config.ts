ac74beb65e259f2c2d9621fab5c16a35f0bf5ae1
Alois Sečkár <ellrohir@seznam.cz>
2024-08-25 08:43:29 +0200

    build: tech update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-08-10',
+  compatibilityDate: '2024-08-25',
 
   devtools: { enabled: false },
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
-    '@vueuse/nuxt',
   ],
 
   eslint: {
