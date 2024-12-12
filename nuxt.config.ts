d4f665b8c9228f6baa917797a40ae3078172d7b5
Alois Sečkár <ellrohir@seznam.cz>
2024-12-12 20:04:14 +0100

    build: switch to Nuxt Ignis dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-08-25',
-
-  devtools: { enabled: false },
-
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui',
+  extends: [
+    'nuxt-ignis'
   ],
 
   eslint: {
